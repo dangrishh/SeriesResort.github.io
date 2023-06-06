4cb40a5aae96df40e7d53a7e143076a100613515
Daniel De Torres <88454993+dangrishh@users.noreply.github.com>
2023-06-06 10:48:00 +0800

    File Uploads

---
+++ orms/rooms.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+
+<style>
+
+    
+@font-face {
+      font-family: 'Poppins-Light';
+      src: url(Poppins-Light.ttf);
+    }
+
+    @font-face {
+      font-family: 'Poppins-Regular';
+      src: url(Poppins-Regular.ttf);
+    }
+
+    body {
+        
+        background: url(uploads/blur-background.png);
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        overflow-x: hidden;
+       
+    }
+    
+    #top-Nav {
+      position: absolute;
+    }
+    #hide-book-button {
+        display: none;
+    }
+    
+    .picture-1 {
+     
+        position: absolute;
+        width: 1591px;
+        height: 758px;
+        left: 166px;
+        top: 210px;
+
+    }
+
+    .picture-2 {
+     
+     position: absolute;
+     width: 1591px;
+     height: 758px;
+     left: 166px;
+     top: 1300px;
+
+    }
+
+    
+    .picture-3 {
+     
+     position: absolute;
+     width: 1591px;
+     height: 758px;
+     left: 166px;
+     top: 2400px;
+
+    }
+
+    .label-1 {
+     position: absolute;
+     
+     left: 450px;
+     top: 3450px;
+    }
+
+    .a1 {
+     position: absolute;
+     
+     left: 80px;
+     top: 3900px;
+    }
+    .a2 {
+     position: absolute;
+     
+     left: 1000px;
+     top: 4200px;
+    }
+    .a3 {
+     position: absolute;
+     
+     left: 80px;
+     top: 4500px;
+    }
+    .a4 {
+     position: absolute;
+     
+     left: 1000px;
+     top: 4900px;
+    }
+    .a5 {
+     position: absolute;
+     
+     left: 80px;
+     top: 5300px;
+    }
+    .a6 {
+     position: absolute;
+     
+     left: 1000px;
+     top: 5670px;
+    }
+    .a7 {
+     position: absolute;
+     
+     left: 80px;
+     top: 6100px;
+    }
+    .a8 {
+     position: absolute;
+     
+     left: 1000px;
+     top: 6400px;
+    }
+    .a9 {
+     position: absolute;
+     
+     left: 80px;
+     top: 6800px;
+    }
+    .a10 {
+     position: absolute;
+     
+     left: 1000px;
+     top: 7100px;
+    }
+
+    .a11 {
+     position: absolute;
+     
+     left: 80px;
+     top: 7500px;
+    }
+    .a12 {
+     position: absolute;
+     
+     left: 600px;
+     top: 7950px;
+    }
+
+    #hide-label, .label-series1 {
+        display: none;
+    }
+
+    #div-visitor {
+      display: none;
+    }
+
+</style>
+
+<body>
+    
+<img src="uploads/p1.png" class="picture-1" data-aos="fade-in" data-aos-duration="1000"> </img>
+<img src="uploads/p2.png"class="picture-2" data-aos="fade-in" data-aos-duration="1500"></img>
+<img src="uploads/p3.png" class="picture-3" data-aos="fade-in" data-aos-duration="1500"></img>
+
+<img src="uploads/amenities-label.png" class="label-1"data-aos="fade-in" data-aos-duration="1500"> </img>
+
+<img src="uploads/a1.png" class="a1" data-aos="slide-right"></img>
+<img src="uploads/a2.png" class="a2" data-aos="slide-left"></img>
+<img src="uploads/a3.png" class="a3" data-aos="slide-right"></img>
+<img src="uploads/a4.png" class="a4" data-aos="slide-left"></img>
+<img src="uploads/a5.png" class="a5" data-aos="slide-right"></img>
+<img src="uploads/a6.png" class="a6" data-aos="slide-left"></img>
+
+<img src="uploads/a7.png" class="a7" data-aos="slide-right"></img>
+<img src="uploads/a8.png" class="a8" data-aos="slide-left"></img>
+<img src="uploads/a9.png" class="a9" data-aos="slide-right"></img>
+
+<img src="uploads/a10.png" class="a10" data-aos="slide-left"></img>
+<img src="uploads/a11.png" class="a11" data-aos="slide-right"></img>
+<img src="uploads/a12.png" class="a12"></img>
+
+
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+
+</body>
+</html>
+
+
+
+
+
